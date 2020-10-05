92133dce43789f7231b0cefa4510af720458e78d
Paul Norton <panorton@sdsc.edu>
2020-10-05 14:34:22 -0700

    Updated mount point, removed /var/tmp moves

--- movefiles.sh
+++ movefiles.sh
@@ -4,8 +4,8 @@ mkdir /mnt/root
 mkdir /mnt/tmp 
 mkdir /mnt/var 
 mkdir /mnt/home
-mount -o nouuid /dev/nvme1n1p1 /mnt/root/
-yum -q -y install lvm2 
+mount -o nouuid /dev/nvme1n1p2 /mnt/root/
+dnf -q -y install lvm2 
 vgchange -a y vg 
 
 mount /dev/mapper/vg-lv_tmp /mnt/tmp/ 
@@ -23,8 +23,6 @@ mv /mnt/root/var/log/audit/* /mnt/var/log/audit/
 rm -rf /mnt/root/var/log/audit/
 mv /mnt/root/var/log/* /mnt/var/log/ 
 rm -rf /mnt/root/var/log/
-mv /mnt/root/var/tmp/* /mnt/var/tmp/
-rm -rf /mnt/root/var/tmp/
 mv /mnt/root/var/* /mnt/var/ 
 mv /mnt/root/home/* /mnt/home/ 
 
@@ -32,7 +30,6 @@ cat << EOF >> /mnt/root/etc/fstab
 /dev/vg/lv_home /home   xfs     defaults,nodev        0 0
 /dev/vg/lv_tmp  /tmp    xfs     rw,nodev,noexec,nosuid        0 0
 /dev/vg/lv_var  /var    xfs     defaults,nodev        0 0
-/dev/vg/lv_var_tmp	/var/tmp	xfs	defaults,nosuid,nodev,noexec	0 0
 /dev/vg/lv_var_log      /var/log        xfs     defaults,nodev        0 0
 /dev/vg/lv_var_log_audit        /var/log/audit  xfs     defaults,nodev        0 0
 none   /dev/shm        tmpfs   defaults,nodev,nosuid,noexec    0 0
