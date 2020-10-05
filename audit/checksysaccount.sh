2b95eeeeb749178f7f2b0c96cde0195b9e84451e
Paul Norton <panorton@sdsc.edu>
2020-10-05 10:47:58 -0700

    Moving testing and dev files to dev branch

--- audit/checksysaccount.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# 5.4.2 Ensure system accounts are non-login
-
-egrep -v "^\+" /etc/passwd | awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $3<1000 && $7!="/sbin/nologin" && $7!="/bin/false") {print}'
